b68a13f99b1ab544a2df7a370f9053b07f5192d8
redds2003 <ravinandanreddy.s@gmail.com>
2026-02-07 17:57:40 +0530

    variables

--- helloworld.sh
+++ helloworld.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 
-echo "hi sampath"
-echo "ramesh: Hello sampath reddy"
-echo "sampath: hey! hi "
-echo "am doing very really good and how about you??"
-echo "ramesh:am also doing good"
+
+PERSON1=Sampath
+PERSON2=Ramesh
+echo "$PERSON2:hi sampath"
+echo "$PERSON2: Hello sampath reddy"
+echo "$PERSON1: hey! hi "
+echo "$PERSON1:am doing very really good and how about you??"
+echo "$PERSON2:am also doing good"
 
