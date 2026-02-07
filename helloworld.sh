9f98f12dca235f8f0a20e30b4b466041913d1beb
redds2003 <ravinandanreddy.s@gmail.com>
2026-02-07 18:08:34 +0530

    variables

--- helloworld.sh
+++ helloworld.sh
@@ -2,11 +2,17 @@
 
 
 
-PERSON1=Bmw
-PERSON2=Volvo
-echo "$PERSON2:hi sampath"
-echo "$PERSON2: Hello sampath reddy"
-echo "$PERSON1: hey! hi "
-echo "$PERSON1:am doing very really good and how about you??"
-echo "$PERSON2:am also doing good"
+# PERSON1=Bmw
+# PERSON2=Volvo
+# echo "$PERSON2:hi sampath"
+# echo "$PERSON2: Hello sampath reddy"
+# echo "$PERSON1: hey! hi "
+# echo "$PERSON1:am doing very really good and how about you??"
+# echo "$PERSON2:am also doing good"
 
+
+
+
+echo "Enter your username::"
+read USERNAME
+echo "User name is $USERNAME"
