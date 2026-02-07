9d29a209279ea457ef36bab0614889e833bdf13c
2005-ravi <ravinandanreddy2005@gmail.com>
2026-02-07 15:16:20 +0530

    hello word created

--- helloworld.sh
+++ helloworld.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+
+echo "hi sampath"
+echo "ramesh: Hello sampath reddy"
+echo "sampath: hey! hi "
+echo "am doing very really good and how about you??"
+echo "ramesh:am also doing good"
+
