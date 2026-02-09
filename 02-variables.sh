a57c35fdbf6f2b22c47760613880dc2db82071c1
redds2003 <ravinandanreddy.s@gmail.com>
2026-02-09 13:23:13 +0530

    actions

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
-echo "script executed at:$TIMESTAMP"
+
+
+STARTTIME=$(date +s%)
+echo "script executed at:$STARTTIME"
+
+sleep 10
+
+ENDTIME=$(date +s%)
+TOTALTIME=$(($STARTTIME - $ENDTIME))
+echo "script executed at:$TOTALTIME"
