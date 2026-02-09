8d4c2ab0f63223eaca078fc60b0b0e614ff0f92d
redds2003 <ravinandanreddy.s@gmail.com>
2026-02-09 17:46:35 +0530

    actions

---
+++ 06-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "please run the program with root user access"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then 
+        echo "$2 ...Failure"
+        exit 1
+    else
+        echo "$2 ...success"
+    fi
+}
+
+dnf install nginx -y
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y
+VALIDATE $? "Installing Mysql"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"
