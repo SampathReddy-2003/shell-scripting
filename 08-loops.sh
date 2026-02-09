3d7175e8c02684fd1828461562df3957e03b2ffe
redds2003 <ravinandanreddy.s@gmail.com>
2026-02-09 19:21:26 +0530

    actions

---
+++ 08-loops.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+
+if [ $USERID -ne 0 ]; then
+    echo "please run the program with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then 
+        echo "$2 ...Failure" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ...success" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ #sudo sh 08-loops.sh nginx mysql nodejs
+do
+    dnf install $package -y &>> $LOGS_FILE
+    VALIDATE $? "$PACKAGE installation"
+
+done
