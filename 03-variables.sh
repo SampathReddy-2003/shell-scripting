f69289e4487292b3ebfbfb2eada44069ca407c60
redds2003 <ravinandanreddy.s@gmail.com>
2026-02-09 14:17:24 +0530

    actions

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,3 +5,12 @@ NUM1=100
 NUM2=200
 SUM=$(($NUM1+$NUM2))
 echo "sum is: $SUM"
+
+#arrays
+
+FRUIT=("APPLE","BANANA","POMO")
+echo "fruits are: ${FRUIT[@]}"
+echo "fruits are: ${FRUIT[0]}"
+echo "fruits are: ${FRUIT[1]}"
+echo "fruits are: ${FRUIT[2]}"
+
