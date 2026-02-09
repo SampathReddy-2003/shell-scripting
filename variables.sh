5ddc758069952f2c008f63d757ce9f9eccf828e5
redds2003 <ravinandanreddy.s@gmail.com>
2026-02-09 12:36:44 +0530

    actions

--- variables.sh
+++ variables.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+
+echo "Please enter your username::"
+read USERNAME
+echo "username is $USERNAME"
+
+echo "please enter your password::"
+read PASSWORD
+echo "password is $PASSWORD"
