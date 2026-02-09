43fe7215ce2bedc48771771a25d3cb938340a48a
redds2003 <ravinandanreddy.s@gmail.com>
2026-02-09 15:33:22 +0530

    actions

--- 04-conditions.sh
+++ 04-conditions.sh
@@ -5,6 +5,9 @@ NUMBER=$1
 if [ $NUMBER -gt 20 ]; then
     echo "Given number: $NUMBER is grater than 20"
 
+elif [ $NUMBER -eq 20]; then
+    echo "Given number: $NUMBER is equal to 20"
+
 else
     echo "given number: $NUMBER is less than 20"
 fi
