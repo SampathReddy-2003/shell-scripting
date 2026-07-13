7ea0b421393828ed3ca3c58d197b2f2b319f29ad
redds2003 <ravinandanreddy.s@gmail.com>
2026-07-13 23:26:27 +0530

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh: Hi, How are you doing?"
+echo "Suresh: Hi ramesh. thanks for asking , Im doing good."
+echo "Ramesh: thats great! and sounds good"
+echo "Suresh: How about you? how are doing?"
+echo "Ramesh: Im doing really good suresh."
