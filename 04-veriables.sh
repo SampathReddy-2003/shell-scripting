a4f6d1b45e127c0e535d2e3f1f158bfbe3a90cef
redds2003 <ravinandanreddy.s@gmail.com>
2026-07-13 23:51:36 +0530

    actions

---
+++ 04-veriables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "please enter your username::"
+read USER_NAME
+echo "user name is $USER_NAME"
+
+echo "please enter your password::"
+read -s PASSWORD
+echo "Password is $PASSWORD"
+
